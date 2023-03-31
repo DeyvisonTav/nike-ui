53b1eca5257b70c64b277f357aa55d8572fdf3ea
Deyvid Tavares <devdeyvid2022@gmail.com>
2023-03-31 19:04:10 -0300

    header - fix: adding animation hover in nav and adding home in nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,26 @@
+import "./styles.css"
+
 import { BiShoppingBag } from "react-icons/bi";
 
 export function HeaderPage() {
   return (
-    <div className="w-full bg-black h-[75px] text-[#d6d6d6] text-xl fixed z-20">
+    <div className="w-full bg-black h-[75px] max-w-full text-[#d6d6d6] overflow-x-hiddenow-x: text-xl fixed z-20">
       <div className="max-w-[1540px] h-full mx-auto flex items-center justify-between">
         <a href="#">
           <img src="/NikeLogo.png" alt="" />
         </a>
 
         <div className="flex items-center gap-[40px]">
-          <a className="hover:text-white hover:duration-500" href="#">
+          <a className="hover:text-white duration-500 nav" href="#">
+            Home
+          </a>
+          <a className="hover:text-white hover:duration-500 nav" href="#">
             Sobre-nós
           </a>
-          <a href="#" className="hover:text-white hover:duration-500">
+          <a href="#" className="hover:text-white hover:duration-500 nav">
             Avaliações
           </a>
-          <a href="#" className="hover:text-white hover:duration-500">
+          <a href="#" className="hover:text-white hover:duration-500 nav">
             Produtos
           </a>
         </div>
